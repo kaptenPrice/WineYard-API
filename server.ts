6b9ee1556cf0f6c3511bdbeef0d51817f5197cfe
kaptenPrice <khazar.mehraban@gmail.com>
2021-09-10 15:29:35 +0200

    Heroku

--- server.ts
+++ server.ts
@@ -28,7 +28,7 @@ app.use(middleware.handle(i18next));
 // app.use(cors())
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ['https://miwine.netlify.app'], credentials: true}));
+app.use(cors({ origin: ['https://miwine.netlify.app', "http://localhost:3000"], credentials: true}));
 app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
